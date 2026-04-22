689374f26c471ccd8e5fb9de1fcfdeaaca695bc7
Miftahul Jannat (JONY) <133002425+jonyahmed1@users.noreply.github.com>
2026-04-22 08:38:23 +0600

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,12 @@
+// Player.java
+public class Player {
+    private String name;
+    private Wallet wallet;
+
+    public Player(String name, Wallet wallet) {
+        this.name = name;
+        this.wallet = wallet;
+    }
+
+    // Getters and other methods...
+}
