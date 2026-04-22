5dfa36d7d70b42095774e57c1642198e7163c773
Miftahul Jannat (JONY) <133002425+jonyahmed1@users.noreply.github.com>
2026-04-22 08:32:46 +0600

    Create Game class framework

---
+++ src/main/java/com/gambling/Game.java
@@ -0,0 +1,22 @@
+package com.gambling;
+
+public class Game {
+    // Attributes of the game can be defined here
+
+    // Constructor
+    public Game() {
+        // Initialize game settings
+    }
+
+    // Method to start the game
+    public void start() {
+        // Code to start the game
+    }
+
+    // Method to stop the game
+    public void stop() {
+        // Code to stop the game
+    }
+
+    // Other game-related methods can be added here
+}
